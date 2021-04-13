39a129f88357d41ed1203c163212094e8210d30b
Ritam Agrawal <ritam.kumari.27@gmail.com>
2021-04-13 00:58:42 -0700

    removing task-1/01.py and task-2/02.py files

--- AssignmentT1_T2/Task-1/01.py
+++
@@ -1,7 +0,0 @@
-# 4. Write a program that takes input from the user and prints it using both Python 2.x and Python 3.x
-# Version.
-idea = input("Enter your Name: ")
-# print (idea)
-# print idea
-
-
