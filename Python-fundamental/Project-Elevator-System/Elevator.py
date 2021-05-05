bcba9e385051219677063f623dfa91b7c866e785
Ritam Agrawal <ritam.kumari.27@gmail.com>
2021-05-05 01:19:23 -0700

    Adding utils package to project Elevator System

--- Python-fundamental/Project-Elevator-System/Elevator.py
+++ Python-fundamental/Project-Elevator-System/Elevator.py
@@ -1,12 +1,15 @@
 from utils.util import Elevator
 
 ele = Elevator()
+
 # Setting Maximum and minimum floor levels in the building 
 Topmost_floor = ele.errorHandlingMaxFloorIntegerValue()
 Bottom_floor = ele.errorHandlingMinFloorIntegerValue()
+
 # current floor value of the user, assuming the elevator is at the current floor
 current = ele.errorHandlingCurrentFloorValue()
 current_floor = ele.gettingValidCurrentFloorValue(current, Bottom_floor, Topmost_floor)
+
 # Destination floor value, to which elevator is supposed to go from the current floor 
 destination = ele.errorHandlingDestinationFloorValue()
 destination_floor = ele.gettingValidDestinationValue(destination, Bottom_floor, Topmost_floor)
